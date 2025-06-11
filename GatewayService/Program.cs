c7a20aac2c933583e2ae634968f0eb04b4be3e96
Shubham Aggarwal <imshubhamaggarwal5121@gamil.com>
2025-06-12 01:03:41 +0530

    Add project files.

---
+++ GatewayService/Program.cs
@@ -0,0 +1,39 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[Gateway] Incoming Request: {context.Request.Method} {context.Request.Path}");
+    await next();
+});
+
+
+app.UseOcelot();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
